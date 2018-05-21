6a74ae2bdf26fbbda0832c3585c6fd0a62e3926f
julius kamau <julius.kamau@zilojo.com>
2018-05-21 21:48:44 +0300

    files

--- js/vendor/app.js
+++ js/vendor/app.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
+    $('#image-gallery').lightSlider({
+        gallery:true,
+        item:1,
+        thumbItem:9,
+        slideMargin: 0,
+        mode: 'slide',
+        auto:true,
+        onSliderLoad: function() {
+           $('#image-gallery').removeClass('cS-hidden');
+        
+        }  
+    });
+});
+
+
+
+
 $(document).foundation();
